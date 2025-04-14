a00e8163fd4b31f73ab81c079ecc536710d2ff49
RPINerd <RPINerd@gmail.com>
2025-04-14 19:22:39 -0400

    Settings reading wasn't working, dropping for now

--- trenchrun/config.py
+++ trenchrun/config.py
@@ -3,50 +3,22 @@ Hardcoded configuration settings for the game.
 
 All distances or sizes are in meters unless otherwise specified.
 """
-import configparser
-from pathlib import Path
-
 from pygame.locals import K_DOWN, K_LEFT, K_RIGHT, K_UP, K_a, K_d, K_s, K_w
-
-# Load settings from the ini file
-config_parser = configparser.ConfigParser()
-ini_file = Path(__file__).parent / "settings.ini"
-try:
-    config_parser.read(ini_file)
-except FileNotFoundError:
-    raise FileNotFoundError(f"Settings ini file not found! Please check the file path: {ini_file}")
-except configparser.Error as e:
-    raise configparser.Error(f"Error reading the ini file: {e}")
-
-
-def get_setting(section: str, key: str, fallback: str) -> str:
-    """
-    Helper function to get values from the ini file with a fallback
-
-    Args:
-        section (str): The section of the ini file
-        key (str): The key in the section
-        fallback (str): The fallback value if the key is not found
-    Returns:
-        str: The value from the ini file or the fallback value
-    """
-    return config_parser.get(section, key, fallback=fallback)
-
 
 VERSION = "1.6_dev"
 
 
 # Window Settings
-FPS = int(get_setting("Game", "FPS", "60"))
-CANVAS_WIDTH = int(get_setting("Display", "ResolutionX", "1024"))
-CANVAS_HEIGHT = int(get_setting("Display", "ResolutionY", "768"))
+FPS = 60
+CANVAS_WIDTH = 1024
+CANVAS_HEIGHT = 768
 CANVAS_CENTER_X = CANVAS_WIDTH // 2
 CANVAS_CENTER_Y = CANVAS_HEIGHT // 2
 CANVAS_CENTER = (CANVAS_CENTER_X, CANVAS_CENTER_Y)
 FONT_STYLE = "font/DeathStar.ttf"
 
 # Trench Settings
-TRENCH_LENGTH = int(get_setting("Game", "TrenchLength", "1500"))
+TRENCH_LENGTH = 700
 TRENCH_WIDTH = 10
 TRENCH_HEIGHT = 10
 WALL_INTERVAL = 20
@@ -57,7 +29,7 @@ EXHAUST_WIDTH = TRENCH_WIDTH / 3.0
 TORPEDO_RANGE = 100
 TORPEDO_RADIUS = 0.25
 TORPEDO_SPAN = 0.7
-LAUNCH_POSITION = EXHAUST_POSITION - TORPEDO_RANGE - 100
+LAUNCH_POSITION = EXHAUST_POSITION - TORPEDO_RANGE - 50
 
 # Parameters for rendering
 DEATH_STAR_RADIUS = CANVAS_HEIGHT * 0.4
@@ -73,7 +45,7 @@ SHIP_HEIGHT_M = 0.8
 
 # Speeds and feeds
 FORWARD_VELOCITY_MS = 60.0
-PROTON_TORPEDO_VELOCITY_MS = FORWARD_VELOCITY_MS * 1.1
+PROTON_TORPEDO_VELOCITY_MS = FORWARD_VELOCITY_MS * 1.8
 VELOCITY_MAX_MS = 15.0
 VELOCITY_DAMPEN = 0.85
 ACCELERATION_MSS = 60.0
